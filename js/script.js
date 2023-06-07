6d82e8ce6c0a0116a44fab1e519a8a02e1253b5e
Tanoooki <schritke@hotmail.com>
2023-06-07 15:44:12 -0300

    responsivo

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown.js';
+import initOutsideClick from './modules/outsideClick.js';
 
 initAccordion();
 initScrollSuave();
 initAnimacaoScroll();
 initTabNav();
 initModal();
+initTooltip();
+initDropdownMenu();
+initOutsideClick();
